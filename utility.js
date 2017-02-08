a8bb8f7c304b69f3eb50f68f00e5ed3e2362891f
Ishan Ratnapala <dragfreeze@gmail.com>
2017-02-08 11:27:26 +0530

    Changed version

--- utility.js
+++ utility.js
@@ -7,7 +7,7 @@ define([
 ], function ($, _, customerData) {
     'use strict';
 
-    var version = '0.0.2';
+    var version = '0.0.3';
     var mobileBreakpoint = 767;
     var tabletBreakpoint = 1023;
     var breakpointNames = {
